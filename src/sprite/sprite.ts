c34fa282e8dd156ef91980f5adf3fb1942cf8145
Joshua Hume <valencelectron@hotmail.com>
2021-04-26 17:04:30 +0900

    sprite types

--- src/sprite/sprite.ts
+++ src/sprite/sprite.ts
@@ -4,15 +4,16 @@ interface Options{
   yPos: number
 }
 
-class Sprite{
+export default class Sprite{
     initial: Options
     shape: string
-    xPos: number
-    yPos: number
-    constructor({shape, xPos, yPos}: Options){
+    xPos?: number
+    yPos?: number
+    constructor({shape, xPos = 0, yPos = 0}: Options){
         this.initial = {shape, xPos, yPos};
         this.shape = shape;
         this.xPos = xPos;
         this.yPos = yPos;
+        Object.freeze(this.initial);
     }
 }
