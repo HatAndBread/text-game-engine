0a1a82a63fa1efbb8313df94a44e70b1ae9d387a
Joshua Hume <valencelectron@hotmail.com>
2021-04-27 18:10:54 +0900

    add fps to sprite

--- src/sprite/sprite.ts
+++ src/sprite/sprite.ts
@@ -1,49 +1,77 @@
-interface Options{
-  xPos?: number
-  yPos?: number
-  color?: string
-  backgroundColor?: string
-  zIndex?: number
+interface Options {
+  xPos?: number;
+  yPos?: number;
+  color?: string;
+  backgroundColor?: string;
+  fps?: number;
+  zIndex?: number;
 }
 
-type Animation = {char: string, color: string, backgroundColor?: string, x: number, y: number}[][]
+type Animation = {
+  char: string;
+  color: string;
+  backgroundColor?: string;
+  x: number;
+  y: number;
+}[][];
 
-export default class Sprite{
-    initial: Options
-    xPos: number
-    yPos: number
-    color: string
-    backgroundColor: string
-    zIndex: number
-    currentFrame: number
-    animations: {[key:string]: Animation}
-    currentAnimation: string | null
-    constructor({xPos = 0, yPos = 0, color = 'black', backgroundColor = 'transparent', zIndex = 0 }: Options){
-        this.initial = {xPos, yPos, color, backgroundColor, zIndex};
-        this.xPos = xPos;
-        this.yPos = yPos;
-        this.color = color;
-        this.backgroundColor = backgroundColor;
-        this.zIndex = zIndex;
-        this.currentFrame = 0;
-        this.animations = {}
-        this.currentAnimation = null;
-        Object.freeze(this.initial);
+export default class Sprite {
+  initial: Options;
+  xPos: number;
+  yPos: number;
+  color: string;
+  backgroundColor: string;
+  zIndex: number;
+  currentFrame: number;
+  fps: number;
+  animations: { [key: string]: Animation };
+  currentAnimation: string | null;
+  constructor({
+    xPos = 0,
+    yPos = 0,
+    color = 'black',
+    backgroundColor = 'transparent',
+    zIndex = 0,
+    fps = 5
+  }: Options) {
+    this.initial = { xPos, yPos, color, backgroundColor, zIndex };
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.color = color;
+    this.backgroundColor = backgroundColor;
+    this.zIndex = zIndex;
+    this.currentFrame = 0;
+    this.fps = fps;
+    this.animations = {};
+    this.currentAnimation = null;
+    Object.freeze(this.initial);
+  }
+  setCurrentAnimation = (name: string) => {
+    if (!this.animations[name]) {
+      throw new Error(`Animation ${name} does not exist on ${this}`);
+    } else {
+      this.currentFrame = 0;
+      this.currentAnimation = name;
     }
-    setCurrentAnimation = (name: string) =>{
-        if (!this.animations[name]){
-            throw new Error(`Animation ${name} does not exist on ${this}`)
-        }else{
-            this.currentFrame = 0;
-            this.currentAnimation = name;
-        }
+  };
+  updateFrame = () => {
+    if (
+      this.currentAnimation &&
+      this.currentFrame < this.animations[this.currentAnimation].length - 1
+    ) {
+      this.currentFrame += 1;
+    } else {
+      this.currentFrame = 0;
     }
-    updateFrame = () => {
-        if (this.currentAnimation && this.currentFrame < this.animations[this.currentAnimation].length - 1){
-            this.currentFrame += 1
-        } else{
-            this.currentFrame = 0}
-        }; 
-    addAnimation = (name: string, animation: {char: string, color: string, backgroundColor?: string, x: number, y: number}[][]) => this.animations[name] = animation;
+  };
+  addAnimation = (
+    name: string,
+    animation: {
+      char: string;
+      color: string;
+      backgroundColor?: string;
+      x: number;
+      y: number;
+    }[][]
+  ) => (this.animations[name] = animation);
 }
-
