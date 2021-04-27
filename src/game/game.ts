ec3627922608073b1e63378965d0ae1e88816828
Joshua Hume <valencelectron@hotmail.com>
2021-04-27 17:10:16 +0900

    clean up

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,96 +1,112 @@
-import Pixel from '../pixel/pixel.js'
-import Sprite from '../sprite/sprite.js'
+import Pixel from '../pixel/pixel.js';
+import Sprite from '../sprite/sprite.js';
 import BRUSHES from '../constants/BRUSHES.js';
-import {listenForKeyboard} from '../keyboard/keyboard.js'
+import { listenForKeyboard } from '../keyboard/keyboard.js';
 
-interface Options  {
-  width?: number
-  height?: number
-  fps?: number
-  pixelSize?: number
-  backgroundColor?: string
-  sprites?: Sprite[] | null
+interface Options {
+  width?: number;
+  height?: number;
+  fps?: number;
+  pixelSize?: number;
+  backgroundColor?: string;
+  sprites?: Sprite[] | null;
 }
 
-export default class Game{
-    width: number
-    height: number
-    fps: number
-    pixelSize: number
-    backgroundColor: string
-    sprites: Sprite[] | null
-    animating: boolean;
-    canvas: HTMLDivElement;
-    rows: HTMLDivElement[][]
+export default class Game {
+  width: number;
+  height: number;
+  fps: number;
+  pixelSize: number;
+  backgroundColor: string;
+  sprites: Sprite[] | null;
+  animating: boolean;
+  canvas: HTMLDivElement;
+  rows: HTMLDivElement[][];
 
-    constructor({width = 100, height = 100, fps = 5, pixelSize = 5, backgroundColor = 'white', sprites = null}: Options){
-        this.width = width;
-        this.height = height;
-        this.fps = fps;
-        this.pixelSize = pixelSize;
-        this.backgroundColor = backgroundColor;
-        this.rows = []
-        this.sprites = sprites
-        this.animating = false;
-        listenForKeyboard()
-        this.canvas = document.createElement('div');
+  constructor({
+    width = 100,
+    height = 100,
+    fps = 5,
+    pixelSize = 5,
+    backgroundColor = 'white',
+    sprites = null
+  }: Options) {
+    this.width = width;
+    this.height = height;
+    this.fps = fps;
+    this.pixelSize = pixelSize;
+    this.backgroundColor = backgroundColor;
+    this.rows = [];
+    this.sprites = sprites;
+    this.animating = false;
+    listenForKeyboard();
+    this.canvas = document.createElement('div');
+  }
+  createCanvas() {
+    this.canvas.style.fontSize = `${this.pixelSize}px`;
+    document.body.appendChild(this.canvas);
+    this.canvas.id = 'game-canvas';
+    this.canvas.style.fontFamily = 'monospace';
+    this.canvas.style.backgroundColor = this.backgroundColor;
+    this.canvas.style.width = 'fit-content';
+    this.canvas.style.lineHeight = '1';
+    for (let i = 0; i < this.height; i++) {
+      const arr = [];
+      const newRow = document.createElement('div');
+      newRow.style.display = 'flex';
+      newRow.style.padding = '0';
+      newRow.style.margin = '0';
+      this.canvas.appendChild(newRow);
+      for (let j = 0; j < this.width; j++) {
+        const newPixel = document.createElement('div');
+        newPixel.style.padding = '0px';
+        newPixel.style.margin = '0px';
+        newPixel.style.height = `${this.pixelSize}px`;
+        newPixel.style.width = `${this.pixelSize - this.pixelSize * 0.4}px`;
+        newPixel.style.textAlign = 'center';
+        newPixel.style.overflow = 'hidden';
+        newRow.appendChild(newPixel);
+        arr.push(newPixel);
+      }
+      this.rows.push(arr);
     }
-    createCanvas(){
-        this.canvas.style.fontSize = `${this.pixelSize}px`
-        document.body.appendChild(this.canvas);
-        this.canvas.id = 'game-canvas';
-        this.canvas.style.fontFamily = 'monospace';
-        this.canvas.style.backgroundColor = this.backgroundColor;
-        this.canvas.style.width = 'fit-content';
-        this.canvas.style.lineHeight = '1';
-        for (let i = 0; i < this.height; i++){
-            const arr = [];
-            const newRow = document.createElement('div');
-            newRow.style.display = 'flex';
-            newRow.style.padding = '0';
-            newRow.style.margin = '0';
-            this.canvas.appendChild(newRow);
-            for (let j = 0; j < this.width; j++){
-                const newPixel = document.createElement('div');
-                newPixel.style.padding = '0px';
-                newPixel.style.margin = '0px';
-                newPixel.style.height = `${this.pixelSize}px`
-                newPixel.style.width = `${this.pixelSize - this.pixelSize*0.4}px`;
-                newPixel.style.textAlign = 'center';
-                newPixel.style.overflow = 'hidden';
-                newRow.appendChild(newPixel);
-                arr.push(newPixel);
+  }
+  startLoop() {
+    this.animating = true;
+    this.loop();
+  }
+  endLoop() {
+    this.animating = false;
+  }
+  private loop = () => {
+    // const stuff = ["▒","▓", ' ', ' '];
+    this.rows.forEach((row) => {
+      row.forEach((el) => {
+        el.textContent = '';
+      });
+    });
+    if (this.sprites) {
+      this.sprites.forEach((sprite) => {
+        sprite.currentAnimation &&
+          sprite.animations[sprite.currentAnimation][
+            sprite.currentFrame
+          ].forEach((char) => {
+            if (
+              this.rows[sprite.yPos + char.y] &&
+              this.rows[sprite.yPos + char.y][sprite.xPos + char.x]
+            ) {
+              const pixel = this.rows[sprite.yPos + char.y][
+                sprite.xPos + char.x
+              ];
+              pixel.textContent = char.char;
+              if (sprite.backgroundColor)
+                pixel.style.backgroundColor = sprite.backgroundColor;
+              pixel.style.color = char.color;
             }
-            this.rows.push(arr);
-        }
+          });
+        sprite.updateFrame();
+      });
     }
-    startLoop(){
-        this.animating = true;
-        this.loop()
-    }
-    endLoop(){
-        this.animating = false;
-    }
-    private loop = () => {
-        // const stuff = ["▒","▓", ' ', ' ']
-        this.rows.forEach((row)=>{
-            row.forEach((el)=>{
-                el.textContent = ''
-            })
-        })
-        if (this.sprites){
-            this.sprites.forEach((sprite)=>{
-                sprite.currentAnimation && sprite.animations[sprite.currentAnimation][sprite.currentFrame].forEach((char)=>{
-                    if (this.rows[sprite.yPos + char.y] && this.rows[sprite.yPos + char.y][sprite.xPos + char.x]){
-                        const pixel = this.rows[sprite.yPos + char.y][sprite.xPos + char.x];               
-                        pixel.textContent = char.char
-                        if (sprite.backgroundColor) pixel.style.backgroundColor = sprite.backgroundColor;
-                        pixel.style.color = char.color;
-                    } 
-                })
-                sprite.updateFrame()
-            })
-        }
-        if (this.animating) window.requestAnimationFrame(this.loop)
-    }
+    if (this.animating) window.requestAnimationFrame(this.loop);
+  };
 }
--- src/keyboard/keyboard.ts
+++ src/keyboard/keyboard.ts
@@ -1,142 +1,148 @@
-const keyDownEvents: {key: keys, callback: ()=> any }[] = []
-const keyUpEvents: {key: keys, callback: ()=> any }[] = []
+const keyDownEvents: { key: keys; callback: () => any }[] = [];
+const keyUpEvents: { key: keys; callback: () => any }[] = [];
+const keysCurrentlyDown: string[] = [];
+
 export const listenForKeyboard = () => {
-    document.addEventListener('keydown', (e)=>{
-        for (let i = 0; i < keyDownEvents.length; i++){
-            if (keyDownEvents[i].key === e.key) keyDownEvents[i].callback();
-        }
-
-    })
-    document.addEventListener('keyup', (e)=>{
-        for (let i = 0; i < keyUpEvents.length; i++){
-            if (keyUpEvents[i].key === e.key) keyUpEvents[i].callback();
-        }
-    })
-}
-
-export const onKeyDown = (key: keys, callback: ()=>any)=>{
-    for (let i = 0; i < keyDownEvents.length; i++){
-        if (keyDownEvents[i].key === key){
-            keyDownEvents.splice(i, 1);
-        }
+  document.addEventListener('keydown', (e) => {
+    if (!keysCurrentlyDown.includes(e.key)) keysCurrentlyDown.push(e.key);
+    for (let i = 0; i < keyDownEvents.length; i++) {
+      if (keyDownEvents[i].key === e.key) keyDownEvents[i].callback();
     }
-    keyDownEvents.push({key, callback})
-}
-
-export const onKeyUp = (key: keys, callback: ()=>any)=>{
-    for (let i = 0; i < keyUpEvents.length; i++){
-        if (keyUpEvents[i].key === key){
-            keyUpEvents.splice(i, 1);
-        }
+  });
+  document.addEventListener('keyup', (e) => {
+    keysCurrentlyDown.forEach((key, index) => {
+      if (key === e.key) keysCurrentlyDown.splice(index, 1);
+    });
+    for (let i = 0; i < keyUpEvents.length; i++) {
+      if (keyUpEvents[i].key === e.key) keyUpEvents[i].callback();
     }
-    keyUpEvents.push({key, callback})
-}
+  });
+};
 
-type keys = "Backspace" |
-"Tab" |
-"Enter" |
-"Shift" |
-"Shift" |
-"Control" |
-"Control" |
-"Alt" |
-"Alt" |
-"Pause" |
-"CapsLock" |
-"Escape" |
-"PageUp" |
-"PageDown" |
-"Home" |
-"End" |
-"ArrowLeft"|
-"ArrowUp"|
-"ArrowRight" |
-"ArrowDown" |
-"PrintScreen" |
-"Insert" |
-"Delete" |
-"1" |
-"0" |
-"2" |
-"3" |
-"4" |
-"5" |
-"6" |
-"7" |
-"8" |
-"9" |
-"a" |
-"b" |
-"c" |
-"d" |
-"e" |
-"f" |
-"g" |
-"h" |
-"i" |
-"j" |
-"k" |
-"l" |
-"m" |
-"n" |
-"o" |
-"p" |
-"q" |
-"r" |
-"s" |
-"t" |
-"u" |
-"v" |
-"w" |
-"x" |
-"y" |
-"z" |
-"Meta" |
-"Meta" |
-"ContextMenu" |
-"0" |
-"1" |
-"2" |
-"3" |
-"4" |
-"5" |
-"6" |
-"7" |
-"8" |
-"9" |
-"*" |
-"+" |
-"-" |
-"." |
-"/" |
-"F1" |
-"F2" |
-"F3" |
-"F4" |
-"F5" |
-"F6" |
-"F7" |
-"F8" |
-"F9" |
-"F10" |
-"F11" |
-"F12" |
-"NumLock" |
-"ScrollLock" |
-"AudioVolumeMute" |
-"AudioVolumeDown" |
-"AudioVolumeUp" |
-"LaunchMediaPlayer" |
-"LaunchApplication1" |
-"LaunchApplication2" |
-";" |
-"=" |
-"," |
-"-" |
-"." |
-"/" |
-"`" |
-"[" |
-"\\" |
-"]" |
-"'" 
+export const onKeyDown = (key: keys, callback: () => any) => {
+  for (let i = 0; i < keyDownEvents.length; i++) {
+    if (keyDownEvents[i].key === key) {
+      keyDownEvents.splice(i, 1);
+    }
+  }
+  keyDownEvents.push({ key, callback });
+};
+
+export const onKeyUp = (key: keys, callback: () => any) => {
+  for (let i = 0; i < keyUpEvents.length; i++) {
+    if (keyUpEvents[i].key === key) {
+      keyUpEvents.splice(i, 1);
+    }
+  }
+  keyUpEvents.push({ key, callback });
+};
+
+type keys =
+  | 'Backspace'
+  | 'Tab'
+  | 'Enter'
+  | 'Shift'
+  | 'Shift'
+  | 'Control'
+  | 'Control'
+  | 'Alt'
+  | 'Alt'
+  | 'Pause'
+  | 'CapsLock'
+  | 'Escape'
+  | 'PageUp'
+  | 'PageDown'
+  | 'Home'
+  | 'End'
+  | 'ArrowLeft'
+  | 'ArrowUp'
+  | 'ArrowRight'
+  | 'ArrowDown'
+  | 'PrintScreen'
+  | 'Insert'
+  | 'Delete'
+  | '1'
+  | '0'
+  | '2'
+  | '3'
+  | '4'
+  | '5'
+  | '6'
+  | '7'
+  | '8'
+  | '9'
+  | 'a'
+  | 'b'
+  | 'c'
+  | 'd'
+  | 'e'
+  | 'f'
+  | 'g'
+  | 'h'
+  | 'i'
+  | 'j'
+  | 'k'
+  | 'l'
+  | 'm'
+  | 'n'
+  | 'o'
+  | 'p'
+  | 'q'
+  | 'r'
+  | 's'
+  | 't'
+  | 'u'
+  | 'v'
+  | 'w'
+  | 'x'
+  | 'y'
+  | 'z'
+  | 'Meta'
+  | 'Meta'
+  | 'ContextMenu'
+  | '0'
+  | '1'
+  | '2'
+  | '3'
+  | '4'
+  | '5'
+  | '6'
+  | '7'
+  | '8'
+  | '9'
+  | '*'
+  | '+'
+  | '-'
+  | '.'
+  | '/'
+  | 'F1'
+  | 'F2'
+  | 'F3'
+  | 'F4'
+  | 'F5'
+  | 'F6'
+  | 'F7'
+  | 'F8'
+  | 'F9'
+  | 'F10'
+  | 'F11'
+  | 'F12'
+  | 'NumLock'
+  | 'ScrollLock'
+  | 'AudioVolumeMute'
+  | 'AudioVolumeDown'
+  | 'AudioVolumeUp'
+  | 'LaunchMediaPlayer'
+  | 'LaunchApplication1'
+  | 'LaunchApplication2'
+  | ';'
+  | '='
+  | ','
+  | '-'
+  | '.'
+  | '/'
+  | '`'
+  | '['
+  | '\\'
+  | ']'
+  | "'";
